d129608f036944af680a125f3ed752bc6fd23c5e
wookkii <wookkii@home>
2021-05-08 09:49:23 +1000

    Try stencil-tailwind plugin

--- src/components/twds-hexagram/twds-hexagram.tsx
+++ src/components/twds-hexagram/twds-hexagram.tsx
@@ -20,9 +20,16 @@ export class TwdsHexagram {
   render() {
     return (
       <Host>
-        <div>
+        <div class="mb-10 w-20">
           { this._lines.map((line) =>
-            <div >{line? '1' : '0'}</div>
+            line ? 
+              <div class="h-3 mb-2 bg-gray-600 w-full"></div>
+             : 
+             <div class="flex mb-2">
+              <div class="h-3 bg-gray-600 flex-1"></div>
+              <div class="h-3 bg-white w-2"></div>
+              <div class="h-3 bg-gray-600 flex-1"></div>
+             </div>
           )}
         </div>
       </Host>
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,12 @@
 import { Config } from '@stencil/core';
+import tailwind from 'stencil-tailwind'
 
 export const config: Config = {
   namespace: 'stencil-tailwind',
   taskQueue: 'async',
+  plugins: [
+    tailwind()
+  ],
   outputTargets: [
     {
       type: 'dist',
@@ -15,5 +19,8 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null // disable service workers
     }
-  ]
+  ],
+  devServer: {
+    reloadStrategy: 'pageReload'
+  }
 };
